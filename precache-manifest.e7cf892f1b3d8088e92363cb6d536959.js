49f5494e4941ad5b14b30c1204800ec2d26a1748
yohren00 <yohren00@gmil.com>
2020-07-14 19:46:29 +0800

    Updates

---
+++ precache-manifest.e7cf892f1b3d8088e92363cb6d536959.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec38606d6b66ad718691473d3fb1db5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5652c9a15e999be2a133",
+    "url": "/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "098036cba1821713a29c",
+    "url": "/my-app/static/css/main.5a86bdb8.chunk.css"
+  },
+  {
+    "revision": "5652c9a15e999be2a133",
+    "url": "/my-app/static/js/2.002f3655.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/my-app/static/js/2.002f3655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098036cba1821713a29c",
+    "url": "/my-app/static/js/main.575df3b3.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
